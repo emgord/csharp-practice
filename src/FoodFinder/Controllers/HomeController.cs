915a37873cdd069469af78c9672d8cb90659c203
Steve Gravrock <sgravrock@pivotal.io>
2016-10-18 10:53:43 -0700

    Set up hello world project
    Signed-off-by: Emily Gordon <egordon@pivotal.io>

---
+++ src/FoodFinder/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodFinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+    }
+}
